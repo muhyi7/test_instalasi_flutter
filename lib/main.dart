35b297c5ddb73e9ec2646c3c07e49f00960af16a
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-01-13 06:30:05 +0700

    Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,27 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My Apps"),),
-        body: Center(
-          child: Text("Halo"),
+          title: Text("My Apps"),
         ),
-
+        body: Center(
+          child: Text(
+            "HaloLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                backgroundColor: Colors.amber,
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 10,
+                fontFamily: 'Roboto',
+                decorationStyle: TextDecorationStyle.wavy,
+                decoration: TextDecoration.lineThrough,
+                decorationColor: Colors.white,
+                decorationThickness: 2),
+          ),
+        ),
       ),
     );
   }
