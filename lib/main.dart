9b9a44c8e3021996015cb1e449b8e3056bbdf10b
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-02-01 19:33:28 +0700

    Visible and Invisible Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
+main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My Apps"),
-        ),
-        body: Center(
-          child: Text(
-            "HaloLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            maxLines: 4,
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                backgroundColor: Colors.amber,
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 10,
-                fontFamily: 'Roboto',
-                decorationStyle: TextDecorationStyle.wavy,
-                decoration: TextDecoration.lineThrough,
-                decorationColor: Colors.white,
-                decorationThickness: 2),
+      home: Scaffold(appBar: AppBar(title: Text("Column"),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            height: 400,
+            width: 400,
+            color: Colors.green,
           ),
-        ),
+          Container(
+            height: 300,
+            width: 300,
+            color: Colors.blue,
+          ),
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.amber,
+          )
+          ,
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          )
+          
+
+        ],
+      )
       ),
     );
   }
