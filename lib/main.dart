a76802f37a4d7c72a543e0e9671e623ecd6ce4dc
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-02-11 18:08:18 +0700

    row and column

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text("Column"),
+      home: Scaffold(appBar: AppBar(title: const Text("Column"),
       ),
       body: Stack(
         children: [
@@ -33,8 +35,6 @@ class MyApp extends StatelessWidget {
             width: 100,
             color: Colors.red,
           )
-          
-
         ],
       )
       ),
